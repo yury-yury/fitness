9ef7eb60ed78507a12420ba8a8b5d38afefa1d89
yury-yury <yury_yury@mail.ru>
2025-01-04 00:25:00 +0300

    create django project

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import time
 from utils import read_training, write_training, write_dairy
 
-print('Input name of training')
+print("Input name of training")
 name_training = input()
 
 training = read_training(name_training)
-write_dairy(f'{time.asctime(time.localtime(time.time()))} Start training {name_training}\n')
+write_dairy(
+    f"{time.asctime(time.localtime(time.time()))} Start training {name_training}\n"
+)
 for i in training:
     for at in range(i.attempts):
-        print('Упражнение ', i.name)
-        print('Подход ', at + 1)
-        print('Предыдущий вес ', i.wedth)
-        print('Введите вес или ничего при прежнем весе')
+        print("Упражнение ", i.name)
+        print(f"Подход {at + 1} из {i.attempts}")
+        print("Предыдущий вес ", i.wedth)
+        print("Введите вес или ничего при прежнем весе")
         weth_1 = input()
-        if weth_1 != '':
+        if weth_1 != "":
             i.wedth = weth_1
-        print('выполните', i.rep, 'повторений')
+        print("выполните", i.rep, "повторений")
         input()
-        write_dairy(f'{time.asctime(time.localtime(time.time()))} Выполнен {at+1} подход к упражнению {i.name}, {i.rep} повторов с весом {i.wedth} кг\n')
-print('Тренировка закончена')
-write_dairy(f'{time.asctime(time.localtime(time.time()))} Finished training {name_training}\n')
+        write_dairy(
+            f"{time.asctime(time.localtime(time.time()))} Выполнен {at+1} подход к упражнению {i.name}, {i.rep} повторов с весом {i.wedth} кг\n"
+        )
+print("Тренировка закончена")
+write_dairy(
+    f"{time.asctime(time.localtime(time.time()))} Finished training {name_training}\n"
+)
 write_training(name_training, training)
--- utils.py
+++ utils.py
@@ -2,38 +2,36 @@ from classes.exercise import Exercise
 
 
 def read_training(name: str) -> list[Exercise]:
-    """
-    """
-    with open(f'{name}.txt', 'r', encoding='utf-8') as file:
+    """ """
+    with open(f"storage/{name}.txt", "r", encoding="utf-8") as file:
         result: list = []
         for line in file:
-            if line != '\n':
-                ex, rep, attempts, wedth = line.split(',')
-                result.append(Exercise(name=ex,
-                                       rep=rep,
-                                       attempts=int(attempts),
-                                       wedth=wedth.strip()))
+            if line != "\n":
+                ex, rep, attempts, wedth = line.split(",")
+                result.append(
+                    Exercise(
+                        name=ex, rep=rep, attempts=int(attempts), wedth=wedth.strip()
+                    )
+                )
 
     return result
 
 
 def write_training(name: str, data: list[Exercise]) -> None:
-    with open(f'{name}.txt', 'w', encoding='utf-8') as file:
+    with open(f"storage/{name}.txt", "w", encoding="utf-8") as file:
         result = str()
         for item in data:
-            w_data = ', '.join([item.name,
-                                item.rep.strip(),
-                                str(item.attempts),
-                                item.wedth])
-            result += w_data + '\n'
+            w_data = ", ".join(
+                [item.name, item.rep.strip(), str(item.attempts), item.wedth]
+            )
+            result += w_data + "\n"
         file.write(result)
 
 
 def write_dairy(data: str) -> None:
-    with open('training_dairy.txt', 'a', encoding='utf-8') as file:
+    with open("storage/training_dairy.txt", "a", encoding="utf-8") as file:
         file.write(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(read_training(1))
-
